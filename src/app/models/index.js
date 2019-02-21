5924a54154ef04092a09803c75d172790c034cfe
Victor Luiz <victorhsl2@hotmail.com>
2019-02-21 16:17:16 -0300

    file upload + auth

--- src/app/models/index.js
+++ src/app/models/index.js
@@ -1,29 +1,34 @@
-'use strict';
+"use strict";
 
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
+const fs = require("fs");
+const path = require("path");
+const Sequelize = require("sequelize");
 const basename = path.basename(__filename);
-const config = require('../../config/database');
+const config = require("../../config/database");
 const db = {};
 
-const sequelize;
-	sequelize = new Sequelize(config.database, config.username, config.password, config);
+const sequelize = new Sequelize(
+	config.database,
+	config.username,
+	config.password,
+	config
+);
 
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-  })
-  .forEach(file => {
-    const model = sequelize['import'](path.join(__dirname, file));
-    db[model.name] = model;
-  });
+fs.readdirSync(__dirname)
+	.filter(file => {
+		return (
+			file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
+		);
+	})
+	.forEach(file => {
+		const model = sequelize["import"](path.join(__dirname, file));
+		db[model.name] = model;
+	});
 
 Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
+	if (db[modelName].associate) {
+		db[modelName].associate(db);
+	}
 });
 
 db.sequelize = sequelize;
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,25 @@
 const express = require("express");
+const multerConfig = require("./config/multer");
+const upload = require("multer")(multerConfig);
 
 const routes = express.Router();
 
-routes.get("/", (req, res) => res.render("auth/signup"));
+const authMiddleware = require("./app/middlewares/auth");
+const guestMiddleware = require("./app/middlewares/guest");
+
+const UserController = require("./app/controllers/UserController");
+const SessionController = require("./app/controllers/SessionController");
+
+routes.get("/", guestMiddleware, SessionController.create);
+routes.post("/signin", SessionController.store);
+
+routes.get("/signup", guestMiddleware, UserController.create);
+routes.post("/signup", upload.single("avatar"), UserController.store);
+
+routes.use("/app", authMiddleware);
+
+routes.get("/app/logout", SessionController.destroy);
+
+routes.get("/app/dashboard", (req, res) => res.render("dashboard"));
 
 module.exports = routes;
